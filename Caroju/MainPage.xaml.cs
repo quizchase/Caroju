ca254665ba841ec0a0bbb4c0feb5586591868fb2
quzch <quizchase@hotmail.com>
2016-11-24 14:13:50 +0800

    添加项目文件。

---
+++ Caroju/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using App3;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.PushNotifications;
+using Windows.UI.Core;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace App3
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+        public Library Library = new Library();
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Library.Back(ref Display);
+        }
+
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            Library.Forward(ref Display);
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Refresh();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Stop();
+        }
+
+        private void ReShare_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mySplit.IsPaneOpen = !mySplit.IsPaneOpen;
+        }
+
+    }
+
+
+}
